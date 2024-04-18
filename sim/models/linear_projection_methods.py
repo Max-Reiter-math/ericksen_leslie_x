89ded9f0f3161f5021a584cc71baf555da03397c
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2024-04-18 14:42:46 +0200

    solver choice

--- sim/models/linear_projection_methods.py
+++ sim/models/linear_projection_methods.py
@@ -9,11 +9,11 @@ from sim.common.error_computation import errornorm
 # dolfinx v0.7
 
 #SECTION - GENERAL METHOD
-def linear_method(experiment, args, projection: bool, use_mass_lumping: bool, postprocess=None, solver_metadata = {"ksp_type": "bcgs", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}):
+def linear_method(experiment, args, projection: bool, use_mass_lumping: bool, postprocess=None, solver_metadata = {"ksp_type": "richardson", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}):
     """
     Good options for the petsc metadata are:
-        {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
-        {"ksp_type": "fbcgs", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
+        {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}  --> works almost perfectly
+        {"ksp_type": "fbcgs", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}    
     """
    
 
