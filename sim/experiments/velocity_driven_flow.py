3bde48bcdf1fedc029518787457fd8c5213cd736
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2025-01-08 20:57:05 +0100

    consolidating diff.. between dg and not dg

--- sim/experiments/velocity_driven_flow.py
+++ sim/experiments/velocity_driven_flow.py
@@ -34,7 +34,8 @@ class velocity_driven_flow:
         self.meshtags = None
 
         # INIT FUNCTIONS WRT DIMENSION
-        d0 = partial(get_d0, dim = self.dim)    
+        d0 = partial(get_d0, dim = self.dim, dh=self.dh)    
+        dbc = partial(get_dbc, dim= self.dim, dh=self.dh)
         v0 = partial(get_v0, dim = self.dim)
         
         # INITIAL CONDITIONS
@@ -42,7 +43,7 @@ class velocity_driven_flow:
 
         # BOUNDARY CONDITIONS
         self.boundary_conditions = [meta_dirichletbc("v", "geometrical", v0,  marker = self.boundary), \
-                                    meta_dirichletbc("d", "geometrical", d0, marker = self.boundary)]
+                                    meta_dirichletbc("d", "geometrical", dbc, marker = self.boundary)]
     
     @property
     def info(self):
@@ -57,11 +58,18 @@ def boundary_3d(x: np.ndarray) -> np.ndarray:
 def boundary_2d(x: np.ndarray) -> np.ndarray:
     return np.logical_or.reduce((np.isclose(x[0], -0.5), np.isclose(x[0], 0.5), np.isclose(x[1], -0.5, np.isclose(x[1], 0.5))))
 
-def get_d0(x: np.ndarray, dim: int) -> np.ndarray:
+def get_d0(x: np.ndarray, dim: int, dh: int)-> np.ndarray:
     if dim not in [2,3]: raise ValueError("Dimension "+str(dim)+" not supported.")
     # x hase shape (dimension, points)
     values = np.zeros((dim, x.shape[1])) # values is going to be the output
-    defects = np.logical_and(np.isclose(x[1],0.0),  np.logical_or( np.isclose(x[0],0.25) , np.isclose(x[0],-0.25)) ) # array of True and False giving the defect locations
+
+    #NOTE - the following makes mostly sense for the DG0 case
+    tol = 0.9*1/dh
+    order = np.inf
+    # array of True and False giving the defect locations
+    defects1 = np.linalg.norm(np.stack((x[0]-0.25,x[1])), ord = order, axis = 0) <= tol
+    defects2 = np.linalg.norm(np.stack((x[0]+0.25,x[1])), ord = order, axis = 0) <= tol
+    defects = np.logical_or(defects1,defects2)
     no_defect = np.invert(defects)
     
     # Setting defects
@@ -79,6 +87,21 @@ def get_d0(x: np.ndarray, dim: int) -> np.ndarray:
     values = values / norms # renormalize
     return values
 
+def get_dbc(x: np.ndarray, dim: int, dh: int)-> np.ndarray:
+    #TODO round up everything that is close enough to boundary
+    if dim not in [2,3]: raise ValueError("Dimension "+str(dim)+" not supported.")
+    # x hase shape (dimension, points)
+    
+    #NOTE - for the DG case we move the interpolation points close enough to the boundary onto the boundary
+    h=1/dh
+    close_to_boundary_dg_dofs = [np.logical_or(x[i] == np.max(x[i]), x[i] == np.min(x[i])) for i in range(dim)]
+    for i in range(dim):
+        x[i][close_to_boundary_dg_dofs[i]] = 0.5 * x[i][close_to_boundary_dg_dofs[i]] / np.abs(x[i][close_to_boundary_dg_dofs[i]])
+    
+    # now reapplying the previous function
+    values = get_d0(x, dim, dh)
+    return values
+     
 def get_v0(x: np.ndarray, dim: int) -> np.ndarray:
     if dim not in [2,3]: raise ValueError("Dimension "+str(dim)+" not supported.")
     # x hase shape (dimension, points)
